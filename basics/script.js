f813db787d912ace7999b2dca324ab03bcca57e5
Bencsik Botond <bencsik.botond@BOLYAI.LOCAL>
2024-11-29 09:37:11 +0100

    Uj functiont hoztunk letre, getTierLevel. Itt If, Else If-el 3 kategoriaba soroljuk a level-t. A vegen Chrome-ban F12-vel a gomszab.getTierLevel(); paranccsal tudjuk megnezni a szintet. A/B/C szintek vannak (Lasd Kod)

--- basics/script.js
+++ basics/script.js
@@ -6,11 +6,28 @@ function Player(nickname){
 }
 const gomszab = new Player('gomszab');
 console.log(gomszab);
+
 Player.prototype.play = function(){
 
     this.playedMatch ++;
     console.log(this.name + "has played");
 }
 
+Player.prototype.getTierLevel = function(){
 
+    if(this.playedMatch <= 3)
+    {
+        return "A";
+    }
+    else if(this.playedMatch >= 3 && this.playedMatch <=6)
+    {
+        return "B";
+    }
+    else
+    {
+        return "C";
+    }
 
+    
+}
+console.log(getTierLevel());
