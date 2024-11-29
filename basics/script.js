1d611e74c9813d1f576078c9e2359219bd5f528d
Bencsik Botond <bencsik.botond@BOLYAI.LOCAL>
2024-11-29 09:18:06 +0100

    Letrehoztunk egy prototype-ot, ahol hozzaadunk egy match-et ha a Chrome F12 Console-on futtatunk egy gomszab.play();-t. A this a function Player-re mutat!

--- basics/script.js
+++ basics/script.js
@@ -4,7 +4,13 @@ function Player(nickname){
     this.playedMatch = 0;
 
 }
-
 const gomszab = new Player('gomszab');
-
 console.log(gomszab);
+Player.prototype.play = function(){
+
+    this.playedMatch ++;
+    console.log(this.name + "has played");
+}
+
+
+
