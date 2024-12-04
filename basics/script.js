63854e565d2b33dce4aae14ec3be580fc3ba93d1
Wrabel B <132589894+WrabelB@users.noreply.github.com>
2024-12-04 23:19:40 +0100

    Feladat vegleges befejezese. Chromeban F12 segitsegevel szemugyre tudjuk venni mizu van, mukodik e menon a kodunk. Es lass csodat igen! Minden ki van commentezve, onnan tudsz tajekozodni!

--- basics/script.js
+++ basics/script.js
@@ -114,3 +114,49 @@ class Animal {  //Letrehozzuk az Os osztalyt, Allatok
         console.log(this.sound);
     }
 }
+
+// Letrehozom a Bird osztalyt, feladathoz hiven
+
+
+class Bird extends Animal { //Bird osztaly, orokli az Animal osztalyt
+    constructor(name, sound) {
+        super(name, sound);
+    }
+    fly() { //Function a Bird classon belul, kiirja hogy repul.
+        console.log("Flying");
+    }
+}
+
+// Letrehozom a Mammal osztalyt feladathoz hiven!
+
+class Mammal extends Animal { //Mammal osztaly orokli az Animal osztaly tulajdonsagait
+    constructor(name, sound) {
+        super(name, sound);
+    }
+    walk() { //Function a Mammalon belul. Kiirja hogy setal
+        console.log("Walking");
+    }
+}
+
+// Feltoltjuk adattal, meghivjuk a class-ainkat. 
+
+//Eloszor a Bird class-t. Mivel az van elol ezzel kezdem, de szerintem tokmindegy
+const rigo = new Bird("Rigo","Csip csip");
+// Johet a Mammal, kutya lett mar mas megint nem jutott eszembe, my bad. Majdnem balnat irtam, mert azt tudom hogy emlos, de rajottem az nem setal...
+const kutya = new Mammal("Kutya","Vau Vau");
+
+
+
+//Hala a csodas F12-nem, latjuk hogy mit dobnak vissza a kulonbozo ertekeink.
+console.log(rigo);
+// Kiirja hgy repul.
+rigo.fly();
+// Kiirja hogy csip csip
+rigo.makeSound();
+
+// Nemirom le megegyszer a fentit, ugyan az ervenyesul itt is!
+console.log(kutya);
+
+kutya.walk();
+
+kutya.makeSound();
