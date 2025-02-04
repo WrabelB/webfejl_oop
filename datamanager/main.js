81de5fb8bf892d9da43e5052abf18f251887b1c3
WrabelB <wrabel.boti@gmail.com>
2025-02-04 17:05:47 +0100

    Hazi feladat elvegezve, Input es addEventListenerek hozzadva, Privat RenderTable letrehozva, sikeresen frissiti az UpdateCallback

--- datamanager/main.js
+++ datamanager/main.js
@@ -47,13 +47,13 @@ class DataManager{
      * 
      * @param {string} name 
      */
-    filterName(name){
+    filterName(name) { // A megadott nev alapjan szuri az adatokat és frissiti a megjelenitest.
         const result = [];
-
-        for(const elem of this.#array){
-            if(elem.nev.includes(name) === name)
-                result.push(name)
-        };
+        for (const person of this.#array) {
+            if (person.nev.includes(name)) {
+                result.push(person);
+            }
+        }
         this.#updatecallback(result);
     }
 
@@ -61,19 +61,26 @@ class DataManager{
      * 
      * @param {number} age 
      */
-    filterAge(age){
+    filterAge(age) { // Az adott eletkorhoz tartozo szemelyeket keresi meg es frissiti a tabalzatot.
         const result = [];
-
-        for(const elem of this.#array){
-            if(elem.eletkor === age)
-                result.push(age)
-        };
+        for (const person of this.#array) {
+            if (person.eletkor === age) {
+                result.push(person);
+            }
+        }
         this.#updatecallback(result);
     }
 }
 
 
 class DataTable{
+    
+
+    /**
+     * @type {HTMLTableSectionElement}
+     */
+    #tbody;
+
     /**
      * 
      * @param {DataManager} dataManager 
@@ -84,26 +91,59 @@ class DataTable{
 
         const tbody = document.createElement('tbody');
         table.appendChild(tbody);
+        this.#tbody = tbody;
 
-        dataManager.setUpdatecallback((persons) => {
-            tbody.innerHTML = "";
+        dataManager.setUpdatecallback((persons) => this.#renderTable(persons)); 
+        //setUpdatecallback függvényt hivjuk meg a DataManageren, es atadunk neki egy fuggvenyt, amely a #renderTable metodust hivja meg a frissitett adatokkal. 
+        // Ez miatt minden valtozas utan automatikusan frissul a tablazat.
+        // A persons tarolja az aktualisan megjelenito szemelyeket tarolo tombot (Person[]).
+    }   
 
-            for(const x of persons){
-                const tr = document.createElement('tr');
-                tbody.appendChild(tr)
 
-                const td = document.createElement('td');
-                td.innerHTML = x.nev;
-                tr.appendChild(td);
+    // Privat renderTable letrehozasa
 
-                const td1 = document.createElement('td');
-                td1.innerHTML = x.eletkor;
-                tr.appendChild(td1);
-            };
-        })
+    /**
+     * @param {Person[]} persons
+     */
+
+    #renderTable(persons) { // Letrehoztam a renderTable fuggvenyt, amivel megjelenitem a tablazatot
+        this.#tbody.innerHTML = "";
+        for (const x of persons) {
+            const tr = document.createElement('tr');
+            this.#tbody.appendChild(tr);
+
+            const td = document.createElement('td');
+            td.innerHTML = x.nev;
+            tr.appendChild(td);
+
+            const td1 = document.createElement('td');
+            td1.innerHTML = x.eletkor;
+            tr.appendChild(td1);
+            
+        }
     }
 }
-
 const dataManager = new DataManager([{nev: 'Józsi', eletkor: 17}, {nev: 'Feri', eletkor: 16}, {nev: 'Gomszab', eletkor: 37}]);
 console.log(dataManager);
 const datatable = new DataTable(dataManager);
+
+// Input mezok letrehozasa es event listenerek
+
+const nameInput = document.createElement('input');
+document.body.appendChild(nameInput);
+
+nameInput.addEventListener('input', () => {
+    dataManager.filterName(nameInput.value);
+});
+
+const ageInput = document.createElement('input');
+ageInput.type = "number";
+document.body.appendChild(ageInput);
+
+
+ageInput.addEventListener('input', () => {
+    const ageValue = Number(ageInput.value);
+    dataManager.filterAge(ageValue);
+});
+
+
